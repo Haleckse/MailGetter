b20017b409a9c3c0be39ff3fe0707451c11bbbdb
Alex Tsan <haleckse@gmail.com>
2025-03-16 12:57:35 +0100

    data base

--- getMail.py
+++ getMail.py
@@ -1,48 +1,58 @@
 import imaplib
 import email
 import imap_tools as it
+import os
+import sqlite3
 from email.header import decode_header
 
-EMAIL = "haleckse@gmail.com"
-PASSWORD = "xaov azte glnk zydh"
+EMAIL = "votre email"
+PASSWORD = "votre mot de passe d'application" # Mot de passe d'application à activer depuis le compte google 
 SERVER = "imap.gmail.com"
 
+print(os.environ)
+pw = os.environ.get(['PW'])
+print(pw)
 
-# Connexion au serveur IMAP (ex: Gmail)
-"""mail = imaplib.IMAP4_SSL("imap.gmail.com")
-mail.login(EMAIL, PASSWORD)
 
-# Sélectionner la boîte de réception
-mail.select("inbox")
 
-# Récupérer les IDs des e-mails
-status, messages = mail.search(None, "ALL")
+connection = sqlite3.connect("email_storage.db")
+cursor = connection.cursor()
+cursor.execute("DROP TABLE emails")
 
-print(mail)
-
-# Décoder et afficher les e-mails
-for num in messages[0].split():
-    status, msg_data = mail.fetch(num, "(RFC822)")
-    raw_email = msg_data[0][1]
-    msg = email.message_from_bytes(raw_email)
-    
-    subject, encoding = decode_header(msg["Subject"])[0]
-    if isinstance(subject, bytes):
-        subject = subject.decode(encoding if encoding else "utf-8")
-    
-    print(f"📩 Sujet : {subject}")
-
-mail.close()
-mail.logout()"""
+# Creation de la base de données
+create_table = """
+CREATE TABLE IF NOT EXISTS emails (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_email TEXT NOT NULL,
+    to_email TEXT NOT NULL,
+    subject TEXT,
+    date_received TEXT NOT NULL,
+    body TEXT
+); """
+cursor.execute(create_table)
 
 
 mb = it.MailBox(SERVER).login(EMAIL, PASSWORD, "Inbox")
 print(mb.folder.get())
 
-for email in mb.fetch(limit=5, reverse=True, mark_seen=True):
+for email in mb.fetch(limit=200, reverse=True, mark_seen=True):
     #print("Title : " + email.subject + "\nContent : " + email.text + "\nDate : " + email.date)
     print(email)
     print('\n')
     print(email.text)
 
-    input("Passer au suivant ?")
+    title = email.subject
+    content = email.text
+    date = email.date
+
+    cursor.execute("""
+            INSERT INTO emails (from_email, to_email, subject, date_received, body)
+            VALUES (?, ?, ?, ?, ?)
+        """, (email.from_, ', '.join(email.to), email.subject, str(email.date), email.text))
+        
+    connection.commit()
+    print("✅ Email enregistré dans la base SQLite !")
+
+    cursor.execute("SELECT subject FROM emails WHERE subject LIKE ?", ('%Call%',))
+    result = cursor.fetchall()
+    print(result)
